814dac2aac85f4566fbf1cb01d16118597c19a94
Hoang Le <hoangledo2092004@gmail.com>
2024-11-17 04:15:27 -0600

    ldh: update stream camera

--- src/pages/Settings/Camera.jsx
+++ src/pages/Settings/Camera.jsx
@@ -1,10 +1,18 @@
-import React from 'react'
+import React, { useRef, useEffect } from 'react'
+import jsmpeg from 'jsmpeg'
+import ReactPlayer from 'react-player'
 
 const Camera = () => {
   return (
     <section>
         <h2 className="text-2xl font-bold m-2">Camera</h2>
-        <div className="w-full h-[150px] border-2 border-gray-300 rounded-2xl"></div>
+        <div className="w-full h-[250px] border-2 border-gray-300">
+          <img
+            src="http://192.168.137.37:8080/video"
+            alt="Live stream"
+            style={{ width: '100%', height: '100%', objectFit: 'cover' }}
+          />
+        </div>
     </section>
   )
 }
