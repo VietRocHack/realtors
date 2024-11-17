dcf9abaa1fb60f9909436e83989765b691a9dda0
Hoang Le <hoangledo2092004@gmail.com>
2024-11-17 06:21:00 -0600

    ldh: integrate stream

--- src/pages/Live/HeatMap.jsx
+++ src/pages/Live/HeatMap.jsx
@@ -1,10 +1,17 @@
 import React from 'react'
 
-const HeatMap = () => {
+const HeatMap = ({ imageUrl }) => {
   return (
     <section>
         <h2 className="text-2xl font-bold mb-4">Eye-tracking Emotion</h2>
-        <div className="w-[300px] h-[300px] bg-gray-200 rounded-2xl"></div>
+        <div className="w-[300px] h-[300px] bg-gray-200 rounded-2xl">
+            (<img
+                src={imageUrl}
+                alt="Heatmap"
+                style={{ width: '100%', height: '100%', objectFit: 'cover' }}
+            />) 
+                
+        </div>
     </section>
   )
 }
--- src/pages/Live/Live.jsx
+++ src/pages/Live/Live.jsx
@@ -1,17 +1,63 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import { useNavigate } from 'react-router-dom'
+import { io } from 'socket.io-client'
+import { PinataSDK } from "pinata"
 import Button from '../../components/Button/Button'
 import HeatMap from './HeatMap'
 import Tips from './Tips'
 
+
 const Live = () => {
   const navigate = useNavigate()
+  const [message, setMessage] = useState(null)
+  const [imageUrl, setImageUrl] = useState("")
+
+  // pinata
+  const pinata = new PinataSDK({
+    pinataJwt: import.meta.env.PINATA_JWT,
+    pinataGateway: import.meta.env.PINATA_GATEWAY,
+  });
+
+  async function fetchImage(id) {
+    try {
+      const data = await pinata.gateways.get(id);
+      console.log(data)
+
+      const url = await pinata.gateways.createSignedURL({
+        cid: id,
+        expires: 1800,
+      })
+      console.log('Image URL:', url)
+      setImageUrl(url)
+    } catch (error) {
+      console.log('Error fetching image:', error);
+    }
+  }
+
+  useEffect(() => {
+    // create a socket connection
+    const socket = io("http://localhost:3000")
+
+    // listen for an event
+    socket.on('message', (data) => {
+      console.log('Received message: ', data)
+      // call the async function
+      fetchImage(data)
+      setMessage(data)
+    })
+
+    // clean up the socket connection
+    return () => {
+      socket.disconnect()
+    }
+  }, [])
+
 
   return (
       <div className="w-[430px] h-[932px] rounded-3xl shadow-lg overflow-hidden flex flex-col items-center justify-center">  
           {/* Main Content */}
           <div className="h-[650px] flex flex-col">
-            <HeatMap />
+            <HeatMap imageUrl={imageUrl}/>
             <br />
             <Tips />
           </div>
