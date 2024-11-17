f57d4e31c8206898079d3a8cbdba4689ebe007e8
Hoang Le <hoangledo2092004@gmail.com>
2024-11-17 10:05:16 -0600

    ldh: final HomePage

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,15 +1,30 @@
 import React from 'react';
 import Button from '../Button/Button';
 import { useNavigate } from 'react-router-dom';
+import { motion } from 'framer-motion';
 
 const NavBar = ({ name, nav }) => {
     const navigate = useNavigate()
     return (
-        <div className="h-1/3 flex justify-center items-center bg-gray-100 rounded-3xl shadow-lg border border-gray-200">
-        <Button onClick={() => navigate(`/${nav}`)} >
-            {name}
-        </Button>
-        </div>
+        <motion.div className="h-1/3 flex flex-col justify-center items-center bg-gradient-to-b from-gray-100 to-white rounded-3xl shadow-[0_-10px_30px_-5px_rgba(0,0,0,0.3)] border border-gray-200"
+                    initial={{ y: '100%' }}
+                    animate={{ y: 0 }}
+                    transition={{ type: 'spring', stiffness: 100, damping: 20 }}
+        >
+            <h2 className="text-4xl font-semibold text-center">Realty <span className="text-emerald-400"> Wingman</span></h2>
+            <p>Best real-time assistant for realtors</p>
+            <p>
+                <span className="text-purple-600"> Voice</span>
+                <span className="text-orange-500"> Eye Gazing</span>
+                <span className="text-blue-400"> Face</span>
+            </p>
+
+            <br />
+
+            <Button onClick={() => navigate(`/${nav}`)} >
+                {name}
+            </Button>
+        </motion.div>
     );
 };
 
--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -8,38 +8,11 @@ const HomePage = () => {
     const navigate = useNavigate()
 
     return (
-        <div className="w-[430px] h-[932px] rounded-3xl shadow-lg overflow-hidden flex flex-col">
-            {/* <header className="mt-32 mb-8">
-                <h1 className="text-4xl font-bold leading-tight text-center">Realty Wingman</h1>
-            </header>
-
-            <main className="flex-grow flex flex-col items-center justify-center px-4">
-                <div className="w-[410px] h-[410px] rounded-lg mb-8">
-                    <img
-                        src={home}
-                        alt="background"
-                        style={{ width: '100%', height: '100%', objectFit: 'cover' }}
-                    /> 
-                </div>
-
-                <div className="flex flex-col items-center space-y-4 mb-16">
-                    <h2 className="text-2xl font-semibold text-center">
-                        Best real-time assistant
-                    </h2>
-                    <h2 className="text-2xl font-semibold text-center">for 
-                        <span className="text-emerald-400"> realtors</span>
-                    </h2>
-                </div>
-
-                <Button onClick={() => navigate('/settings')}>
-                    Explore
-                </Button>
-            </main> */}
+        <div className="w-[430px] h-[932px] rounded-3xl shadow-lg overflow-hidden flex flex-col relative">
             <div className="flex-1">
-                <img src={home}/>
+                <img src={home} alt="Home" className="w-full h-full object-cover" />
             </div>
             <NavBar name="Explore" nav="settings" />
-
         </div>
     )
 }
