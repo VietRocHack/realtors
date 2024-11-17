e66faeb76f0735bb6c6d818f36d2293ea17f9d27
Hoang Le <hoangledo2092004@gmail.com>
2024-11-17 09:48:13 -0600

    ldh: update homepage

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,22 +1,22 @@
 import React from 'react'
 import Button from '../../components/Button/Button'
 import { useNavigate } from 'react-router-dom'
-import buildings from '../../assets/buildings.png'
+import home from '../../assets/home.jpg'
+import NavBar from '../../components/NavBar/NavBar'
 
 const HomePage = () => {
     const navigate = useNavigate()
 
     return (
         <div className="w-[430px] h-[932px] rounded-3xl shadow-lg overflow-hidden flex flex-col">
-            <header className="mt-32 mb-8">
+            {/* <header className="mt-32 mb-8">
                 <h1 className="text-4xl font-bold leading-tight text-center">Realty Wingman</h1>
             </header>
 
             <main className="flex-grow flex flex-col items-center justify-center px-4">
-                {/* Placeholder for image */}
                 <div className="w-[410px] h-[410px] rounded-lg mb-8">
                     <img
-                        src={buildings}
+                        src={home}
                         alt="background"
                         style={{ width: '100%', height: '100%', objectFit: 'cover' }}
                     /> 
@@ -31,15 +31,15 @@ const HomePage = () => {
                     </h2>
                 </div>
 
-                {/* Start button */}
                 <Button onClick={() => navigate('/settings')}>
                     Explore
                 </Button>
-            </main>
+            </main> */}
+            <div className="flex-1">
+                <img src={home}/>
+            </div>
+            <NavBar name="Explore" nav="settings" />
 
-            <footer className="mb-16">
-            {/* Add any footer content here if needed */}
-            </footer>
         </div>
     )
 }
